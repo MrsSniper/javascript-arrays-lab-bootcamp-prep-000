0f73971fb87c069844e67a7f3aef61b3a5e273f0
Nessa S <bnvsnipe@gmail.com>
2018-03-24 03:23:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(`${name}`)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(`${name}`)
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function appendKitten(name) {
+  var newKitten = [...kittens, `${name}`]
+  return newKitten
+}
+
+function prependKitten(name) {
+  var newKitten = [`${name}`, ...kittens]
+  return newKitten
+}
